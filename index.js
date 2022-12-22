af004e62d1553617838657f7e03dae0f431d7bdb
Juan2805 <juan_nacho_92@hotmail.com>
2022-12-22 20:01:29 -0300

    :sparkles: Add functionality to Cart resume and Cart detail

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 var allWines = []
-
-const showCartResume = () => {
-    const cartResumeContainer = document.querySelector('.cart-resume-container')
-    const shoppingCartImageContainer = document.querySelector('.shopping-cart-image-container')
-
-    shoppingCartImageContainer.addEventListener('click', () => {
-        cartResumeContainer.classList.toggle('hidden')
-    })
-}
-
-showCartResume()
-
-const cleanProductListener = () => {
-    const clenaProductButtons = document.querySelectorAll('.fa-trash-can')
-    
-    clenaProductButtons.forEach(clenaProductButton => {
-        clenaProductButton.addEventListener('click', cleanProduct => {
-            let asd = document.querySelector(`#${cleanProduct.target.id}Container`).remove()
-            console.log(asd)
-        })
-    }) 
-}
-
-cleanProductListener()
+var cartProducts = []
 
 const getWines = async () => {
     var response = await fetch('./db.json')
@@ -32,34 +9,52 @@ const getWines = async () => {
 }
 
 const wineCard = (wine) => {
-    return `<div class="card">
-                <p>${wine.tipo}</p>
+    return  `<div class="card">
                 <div class="card__body">
                     <div class="card__img">
-                        <img src="./assets/img/vino1.jpg" alt="Producto">
+                        <img
+                        src="./assets/img/products/botella_gaudila2013.svg"
+                        alt="Producto"
+                        />
                     </div>
                     <div class="card__info">
                         <h2>${wine.marca}</h2>
-                        <h3>Id: ${wine.id}</h3>
-                        <h2>$ ${wine.precio}</h2>
-                        <h2>Description</h2>
-                        <p>${wine.descripcion}</p>
-                        <h2>Avalaible: 20</h2>
-                        <a href="">More...</a>
+                        <h2>${wine.tipo}</h2>
+                        <a href="./pages/detail.html"><button value='${wine.marca}' class="card__button">Conocelo...</button></a>
                     </div>
                 </div>
             </div>`
 }
 
+const cardButtonListeners = () => {
+    const cardButtonsContainers = document.querySelectorAll('.card__button')
+    
+    cardButtonsContainers.forEach(cardButton => {
+        cardButton.addEventListener('click', (event) => {
+            allWines.map(wine => {
+                wine.marca == event.target.value ? localStorage.setItem('myEvent', JSON.stringify(wine)) : null
+            })
+        })
+    })
+}
+
+
+
 const renderAllWines = async () => {
     const cardsContainer = document.querySelector('.cards-container')
 
+    //localStorage.clear()
+    CartProductsExist = JSON.parse(localStorage.getItem('CartProducts'))
+    
+    CartProductsExist ? null : localStorage.setItem('CartProducts', JSON.stringify(cartProducts))
+    console.log(CartProductsExist)
+
     allWines = await getWines()
 
-    // allWines.map(wine => {
-    //     cardsContainer.insertAdjacentHTML('beforeend', wineCard(wine))
-    // })
-    
+    allWines.map(wine => {
+        cardsContainer.insertAdjacentHTML('beforeend', wineCard(wine))
+    })
+    cardButtonListeners()
 }
 
 renderAllWines()
