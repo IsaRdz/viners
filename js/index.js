be0d700cbfe100ab8b31eb9564b7bd6f066cbd04
IsaRdz <isabelrodriguez.dg@gmail.com>
2022-12-28 08:37:36 -0300

    :sparkles: Added top button

--- js/index.js
+++ js/index.js
@@ -105,3 +105,19 @@ const addToCartListener = () => {
         })
     })
 }
+
+const buttonTop = document.querySelector('#buttonTop');
+
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 100) {
+        buttonTop.classList.add('shows')
+    } else {
+        buttonTop.classList.remove('shows')
+    }
+    buttonTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+}
