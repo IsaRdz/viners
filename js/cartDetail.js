2bc54e4da8eab4207aa56af0381ecd4cb9b807cf
TrostdorfA <sebatros@hotmail.com>
2023-01-05 20:53:45 -0300

    :art: Fix: Improving footer style, linking media queries

--- js/cartDetail.js
+++ js/cartDetail.js
@@ -170,3 +170,41 @@ const renderTotalProductNumber = () => {
 }
 
 renderTotalProductNumber()
+
+function copyCoupon() {
+    let copyTxt = document.getElementById("input1");
+    let btnTxt = document.getElementById("btn1");
+
+    copyTxt.select();
+    copyTxt.setSelectionRange(0,99999);
+
+    navigator.clipboard.writeText(copyTxt.value);
+
+    if (btnTxt.innerHTML === "copiar cupon") {
+        btnTxt.innerHTML = "cupon copiado";
+    }
+}
+
+function canjearCoupon() {
+    let canjearTxt = document.getElementById("input2").value;
+    let canjearBtn = document.getElementById("btn2");
+
+    let number = document.getElementById("price").innerHTML;
+    let discount = document.getElementById("discount");
+
+    function getPercent(percent) { return number / 100 * percent; }
+    let percentResult = getPercent(90); //VOY A OBTENER EL 90 PORCIENTO DE 200.
+
+    if (canjearTxt === "54ldqwer23") {
+        canjearBtn.innerHTML = "cupon canjeado";
+
+        discount.innerHTML = `<h3 id="number2">${percentResult}</h3><h3>$</h3><span>${number}$</span>`;
+
+    } else if (canjearTxt === "") {
+
+        discount.innerHTML = '<h3 style="font-size: 15px; width: 300px;">Aún no has pegado el código 🤔</h3>';
+         
+    } else if (canjearTxt !== "54ldqwer23") {
+        discount.innerHTML = '<h3 style="font-size: 15px; width: 300px;">El cupón no es válido 🥲</h3>';
+    } 
+}
